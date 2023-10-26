a7a00abdf53e61e8d99b2f2a5175181fc70335ec
shree1003 <shradheykhatekar20@gmail.com>
2023-10-26 12:29:23 +0000

    done

--- ProdMvc/Controllers/ProductController.cs
+++ ProdMvc/Controllers/ProductController.cs
@@ -17,4 +17,46 @@ public class ProductController : Controller{
         var data=context.Product.Find(id);
         return View(data);
     }
+
+    public IActionResult Create(){
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Product prod){
+        if(ModelState.IsValid){
+            context.Product.Add(prod);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+        return View();
+    }
+
+    public IActionResult Edit(int id){
+        var data=context.Product.Find(id);
+        return View(data);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(Product prod){
+        if(ModelState.IsValid){
+            Product product=context.Product.Find(prod.Id);
+            product.Name=prod.Name;
+            product.Price=prod.Price;
+            product.Stock=prod.Stock;
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+        return View();
+    }
+    public IActionResult Delete(int id){
+        var data=context.Product.Find(id);
+        return View(data);
+    }
+    [HttpPost]
+    public IActionResult Delete(Product prod){
+        Product product=context.Product.Find(prod.Id);
+        context.Product.Remove(product);
+        context.SaveChanges();
+        return RedirectToAction("List"); 
+    }
 }
