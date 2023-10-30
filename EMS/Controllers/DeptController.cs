3c796462a3402838b27e8ed60dfd1cb9ea842372
Iamneo <techadmin@iamneo.ai>
2023-10-30 06:49:22 +0000

    Backup Commit

--- EMS/Controllers/DeptController.cs
+++ EMS/Controllers/DeptController.cs
@@ -1,6 +1,11 @@
+using EMS.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace EMS.Controllers;
 public class DeptController:Controller{
+    private readonly EmsDbContext context;
+    public DeptController(EmsDbContext _context){
+        context = _context;
+    }
 public IActionResult List(){
     return View();
 }
--- EMS/Models/Department.cs
+++ EMS/Models/Department.cs
@@ -7,8 +7,12 @@ namespace EMS.Models;
 public partial class Department
 {
     public int Id { get; set; }
+    [Display(Name ="Department Name")]
+    [Required(ErrorMessage ="Department Name Cannot Blank")]
 
     public string DeptName { get; set; } = null!;
+    [Display(Name ="City")]
+    [StringLength(25,MinimumLength =3, ErrorMessage ="City Must be 3 Chars")]
 
     public string Location { get; set; } = null!;
 
