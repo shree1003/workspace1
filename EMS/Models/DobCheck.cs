146a3138acca3ba3a3ad0dde98b473e62d77c742
shree1003 <shradheykhatekar20@gmail.com>
2023-10-31 09:01:24 +0000

    2.30 pm

--- EMS/Models/DobCheck.cs
+++ EMS/Models/DobCheck.cs
@@ -5,6 +5,12 @@ namespace ems.Models;
 public class DobCheck:ValidationAttribute{
     public override bool IsValid(object value)
     {
-
+        DateTime birthdate=Convert.ToDateTime(value);
+        int year=birthdate.Year;
+        int currentyear=DateTime.Now.Year;
+        if(currentyear - year >=25)
+            return true;
+        else    
+            return false;
     }
 }
