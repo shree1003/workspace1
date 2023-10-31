e0e6fefa06e9ecdfcff48f9c6cb7d1439e96f1c1
shree1003 <shradheykhatekar20@gmail.com>
2023-10-31 04:03:01 +0000

    oct 31 10 am

--- EMS/Controllers/EmpController.cs
+++ EMS/Controllers/EmpController.cs
@@ -1,7 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 namespace EMS.Controllers;
 public class EmpController:Controller{
-public IActionResult List(){
-    return View();
-}
+    private readonly EmsDbContext context;
+    public EmpController(EmsDbContext _context){
+        context= _context;
+    }
+    public IActionResult List(){
+        var data=context.Employees.ToList();
+        return View(data);
+    }
+
+    public IActionResult Create(){
+        ViewBag.DeptId= new SelectList(context.Departments,"Id","DeptName");
+        return View();
+    }
+    public IActionResult Create(Employee emp){
+        if(ModelState.IsValid){
+            context.Employees.Add(emp);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+        ViewBag.DeptId= new SelectList(context.Departments,"Id","DeptName");
+        return View(emp);
+    }
 }
