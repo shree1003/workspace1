83a38eaae7f02036b7db71264b1c85edd0250322
shree1003 <shradheykhatekar20@gmail.com>
2023-10-31 08:56:38 +0000

    31 oct 2 pm

---
+++ EMS/Models/DobCheck.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ems.Models;
+
+public class DobCheck:ValidationAttribute{
+    public override bool IsValid(object value)
+    {
+
+    }
+}
